45c19d25713c8a4ed5795fa0790af4a20110f68d
shaileshkamble122 <shaileshkamble122@gmail.com>
2024-10-06 13:37:33 +0530

    hello world code is updated

--- hello world/hello world/Program.cs
+++ hello world/hello world/Program.cs
@@ -11,6 +11,7 @@ namespace hello_world
         static void Main(string[] args)
         {
             Console.WriteLine("hello, world!");
+            Console.WriteLine("how r u? ");
         }
     }
 }
